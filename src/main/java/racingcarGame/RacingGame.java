4778be2b253c0a15a9a588dcc50ca39afc0c516f
bella.n <bella.n@kakaocorp.com>
2022-10-02 23:06:57 +0900

    docs: 불 필요한 주석 제거

--- src/main/java/racingcarGame/RacingGame.java
+++ src/main/java/racingcarGame/RacingGame.java
@@ -10,16 +10,7 @@ import static racingcarGame.client.InputView.scanCarCount;
 import static racingcarGame.client.InputView.scanPlayCount;
 import static racingcarGame.client.ResultView.showResult;
 
-/**
- * 컨트롤 - 조종
- * 모델을 어쩌구저쩌구
- * 뷰에다가 표시해
- * <p>
- * 자동차 게임을 개최한다. (컨)
- * 자동차(모델)가 주사위를 던지고 당첨되면 1칸 앞으로! (모델이 일을 한다 - oop)
- * n번 시행 - 컨
- * 화면에 표시. - 컨트롤러가 모델을 뷰에다가 던져서 표시하도록
- */
+
 public class RacingGame {
     public static void main(String[] args) {
         List<Car> cars = readyCars();
